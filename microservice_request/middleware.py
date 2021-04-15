3b888247d8ac12fd9b92b547104c044b29d89e97
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-04-15 16:06:51 +0300

    fix bug for remote user

--- microservice_request/middleware.py
+++ microservice_request/middleware.py
@@ -5,6 +5,6 @@ class RemoteUserMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.user_id = None
         if user_id := request.headers.get('Remote-User'):
-            request.remote_user = int(user_id)
+            request.remote_user = int(user_id) if not user_id == 'None' else None
         elif request.user.is_authenticated:
             request.remote_user = request.user.id
