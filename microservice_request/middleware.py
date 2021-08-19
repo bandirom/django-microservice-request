32e1bc05ed4f76ed29c9abb432413129ed8aaa8f
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-08-19 14:59:45 +0300

    added SimpleLazyObject for auth user

--- microservice_request/middleware.py
+++ microservice_request/middleware.py
@@ -1,10 +1,11 @@
 from django.utils.deprecation import MiddlewareMixin
+from django.utils.functional import SimpleLazyObject
 
 
 class RemoteUserMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        request.user_id = None
+        request.remote_user = None
         if user_id := request.headers.get('Remote-User'):
             request.remote_user = int(user_id) if user_id.isdigit() else None
         elif request.user.is_authenticated:
-            request.remote_user = request.user.id
+            request.remote_user = SimpleLazyObject(lambda: request.user.id)
