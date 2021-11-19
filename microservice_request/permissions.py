1439cc4ce9f552ec3d4a4c6a480ab55aa297db3f
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-11-19 16:54:07 +0200

    deleted

--- microservice_request/permissions.py
+++ microservice_request/permissions.py
@@ -12,14 +12,3 @@ class HasApiKeyOrIsAuthenticated(IsAuthenticated):
             if token[0] == settings.API_KEY_HEADER and token[1] == settings.API_KEY:
                 return True
         return super().has_permission(request, view)
-
-
-class HashInHeaderOrIsAdmin(IsAdminUser):
-    methods = ['POST', 'PUT', 'PATCH']
-
-    def has_permission(self, request, view):
-        header_hash = request.headers.get(getattr(settings, 'HASH_HEADER', 'X-Hash'))
-        data_hash: str = md5(dumps(request.data).encode('utf-8')).hexdigest()
-        if request.method in self.methods:
-            return header_hash == data_hash
-        return True
